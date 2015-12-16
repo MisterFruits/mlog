664d51cce5f8c5417a3da880c4d617f1851ff1c9
Victor <vic.toad.tor@free.fr>
2015-12-16 23:46:45 +0100

    NEW tests

--- test_mlog.py
+++ test_mlog.py
@@ -1,6 +1,5 @@
-import mlog
-import random
-from nvd3 import stackedAreaChart, pieChart
+import mlog, random, math, time, datetime
+from nvd3 import stackedAreaChart, pieChart, multiBarChart
 
 def test_LogParser_parse():
     parser = mlog.LogParser(r'(?P<date>\S+) (?P<uid>\S+) (?P<module>\S+) (?P<version>\S+)')
@@ -33,7 +32,7 @@ def test_vincent():
     for cat in cats:
         multi_iter1[cat] = [random.randint(10, 100) for x in index]
 
-    chart = stackedAreaChart(name='stackedAreaChart', height=400, width=800)
+    chart = stackedAreaChart(name='stackedAreaChart', height=400, width=1000)
     extra_serie = {"tooltip": {"y_start": "There is ", "y_end": " min"}}
     for cat in (cat for cat in multi_iter1 if cat != 'index') :
         chart.add_serie(name=cat, x=multi_iter1['index'], y=multi_iter1[cat], extra=extra_serie)
@@ -43,7 +42,7 @@ def test_vincent():
 
 def test_nvd3():
     type = 'pieChart'
-    chart = pieChart(name=type, color_category='category20c', height=450, width=450)
+    chart = pieChart(name=type, color_category='category20c', height=400, width=1050)
     chart.set_containerheader("\n\n<h2>" + type + "</h2>\n\n")
 
     xdata = ["Orange", "Banana", "Pear", "Kiwi", "Apple", "Strawberry", "Pineapple"]
@@ -56,7 +55,7 @@ def test_nvd3():
     write(chart, 'pie')
 
 def test_nvd3_stacked_area():
-    chart = stackedAreaChart(name='stackedAreaChart', height=400, width=400)
+    chart = stackedAreaChart(name='stackedAreaChart', height=400, width=1000)
 
     xdata = [100, 101, 102, 103, 104, 105, 106,]
     ydata = [6, 11, 12, 7, 11, 10, 11]
@@ -69,6 +68,37 @@ def test_nvd3_stacked_area():
 
     write(chart, 'stack')
 
+def test_nvd3_date():
+    type = "multiBarChart"
+
+    chart = multiBarChart(name=type, height=400, width=1000, x_is_date=True)
+    chart.set_containerheader("\n\n<h2>" + type + "</h2>\n\n")
+
+    nb_element = 100
+    start_time = int(time.mktime(datetime.datetime(2013, 6, 1).timetuple()) * 1000)
+    xdata = range(nb_element)
+    xdata = list(map(lambda x: start_time + x * 100000000, xdata))
+    ydata = [i + random.randint(1, 10) for i in range(nb_element)]
+    ydata2 = map(lambda x: x * 2, ydata)
+
+    tooltip_date = "%d %b %Y %H:%M:%S %p"
+    extra_serie = {"tooltip": {"y_start": "There are ", "y_end": " calls"},
+                   "date_format": tooltip_date}
+
+    chart.add_serie(name="Count", y=ydata, x=xdata, extra=extra_serie)
+
+    extra_serie = {"tooltip": {"y_start": "There are ", "y_end": " duration"},
+                   "date_format": tooltip_date}
+    chart.add_serie(name="Duration", y=ydata2, x=xdata, extra=extra_serie)
+    chart.buildhtml()
+
+    write(chart, "date")
+
+
+def datagen(x, ymin, ymax, nsteps, lambdaa=5):
+    assert 0 < x < nsteps, 'x must be posive and lower than nsteps'
+    for x in range(-x, nsteps-x):
+        yield 1/(1+math.exp(x))
 
 def write(chart, filename):
      with open(filename + '.html', 'w') as output_file:
@@ -78,3 +108,4 @@ if __name__ == '__main__':
     test_nvd3_stacked_area()
     test_nvd3()
     test_vincent()
+    test_nvd3_date()
